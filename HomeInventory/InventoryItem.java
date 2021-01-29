bfa9ae16e480c70a2085227be9e31afc354b5f1f
Kratika1616 <70875021+Kratika1616@users.noreply.github.com>
2021-01-29 12:32:44 +0530

    Add files via upload

---
+++ HomeInventory/InventoryItem.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class InventoryItem {
+
+    String description;
+    String location;
+    String serialNumber;
+    boolean marked;
+    String purchasePrice;
+    String purchaseDate;
+    String purchaseLocation;
+    String note;
+    String photoFile;
+
+    public InventoryItem() {
+    }
+    
+}
